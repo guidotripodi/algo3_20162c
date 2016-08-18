157c03be39e89761482ed0ae9d3804ac02ad59dd
Esteban Rey <esteban@bitflowlabs.com>
2016-08-18 20:34:12 -0300

    Se avanza con el desarrollo: falta testear la funcion que procesa el input y finalizar la toma de tiemppos desde Escenario, luego intentar que compile

--- tp1/Escenario.cpp
+++ tp1/Escenario.cpp
@@ -1,81 +1,228 @@
 class Escenario {
-	private:
-		int * parActual_x_paso;
-		int pares_totales = 0;
+private:
+	int * parActual_x_paso;
+	int pares_totales = 0;
 
-		int * personas_ladoA;
-		int cant_canibales_ladoA;
-		int cant_arqueologos_ladoA;
+	int * personas_ladoA;
+	int cant_canibales_ladoA;
+	int cant_arqueologos_ladoA;
 
-		int cant_canibales_ladoB;
-		int cant_arqueologos_ladoB;
+	int cant_canibales_ladoB;
+	int cant_arqueologos_ladoB;
 
-		bool parValido(int par) const
-		{
-			int a  = this.primer(par);
-			int b  = this.segundo(par);
+	bool parValido(int par) const
+	{
+		int a  = this.primer(par);
+		int b  = this.segundo(par);
 
 			//Me fijo que ambos esten presentes en el lado A
-			if (!(personas_ladoA[a] && personas_ladoA[b]))
-			{
-				return false;
-			}
-
-			//Calculo cuantos quedarian de cada lado
-			int cant_canibales_A =  this.cant_canibales_ladoA - this.isCanibal(a) - this.isCanibal(b);
-			int cant_arqueologos_A =  this.cant_canibales_ladoA - !this.isCanibal(a) - !this.isCanibal(b);
-			int cant_canibales_B =  this.cant_canibales_ladoB + this.isCanibal(a) + this.isCanibal(b);
-			int cant_arqueologos_B =  this.cant_canibales_ladoAB + !this.isCanibal(a) + !this.isCanibal(b);
-
-			return cant_canibales_A <= cant_arqueologos_A && cant_canibales_B<=cant_arqueologos_B;
-			
-
-
+		if (!(personas_ladoA[a] && personas_ladoA[b]))
+		{
+			return false;
 		}
 
-		bool faroleroValido(int persona) const
-		{
+			//Calculo cuantos quedarian de cada lado
+		int cant_canibales_A =  this.cant_canibales_ladoA - this.isCanibal(a) - this.isCanibal(b);
+		int cant_arqueologos_A =  this.cant_canibales_ladoA - !this.isCanibal(a) - !this.isCanibal(b);
+		int cant_canibales_B =  this.cant_canibales_ladoB + this.isCanibal(a) + this.isCanibal(b);
+		int cant_arqueologos_B =  this.cant_canibales_ladoAB + !this.isCanibal(a) + !this.isCanibal(b);
+
+		return cant_canibales_A <= cant_arqueologos_A && cant_canibales_B<=cant_arqueologos_B;
+
+
+
+	}
+
+	bool faroleroValido(int persona) const
+	{
 			//Me fijo que este del lado B
-			if(persona_ladoA[persona])
-			{
-				return false;
-			}
+		if(persona_ladoA[persona])
+		{
+			return false;
+		}
 
 			//Calculo cuantos quedarian de cada lado
-			int cant_canibales_A =  this.cant_canibales_ladoA - this.isCanibal(a);
-			int cant_arqueologos_A =  this.cant_canibales_ladoA - !this.isCanibal(a);
-			int cant_canibales_B =  this.cant_canibales_ladoB + this.isCanibal(a);
-			int cant_arqueologos_B =  this.cant_canibales_ladoAB + !this.isCanibal(a);
+		int cant_canibales_A =  this.cant_canibales_ladoA - this.isCanibal(a);
+		int cant_arqueologos_A =  this.cant_canibales_ladoA - !this.isCanibal(a);
+		int cant_canibales_B =  this.cant_canibales_ladoB + this.isCanibal(a);
+		int cant_arqueologos_B =  this.cant_canibales_ladoAB + !this.isCanibal(a);
 
-			return cant_canibales_A <= cant_arqueologos_A && cant_canibales_B<=cant_arqueologos_B;
-		}
+		return cant_canibales_A <= cant_arqueologos_A && cant_canibales_B<=cant_arqueologos_B;
+	}
 
-		bool isCanibal(int persona) const
-		{
+	bool isCanibal(int persona) const
+	{
 			//En la estructura, se listan primero los arqueologos y despues los canibales, ppor lo cual es un canibal si esta en los indices m{s altos
-			return persona > this.cant_arqueologos_ladoA + this.cant_arqueologos_ladoB - 1;
+		return persona > this.cant_arqueologos_ladoA + this.cant_arqueologos_ladoB - 1;
+	}
+
+	int primero(int par) const
+	{
+		int cant_personas = this.cant_arqueologos_ladoA + this.cant_arqueologos_ladoB + this.cant_canibales_ladoA + this.cant_canibales_ladoB
+		return par \ cant_personas;
+	}
+
+	int segundo(int par) const
+	{
+		int cant_personas = this.cant_arqueologos_ladoA + this.cant_arqueologos_ladoB + this.cant_canibales_ladoA + this.cant_canibales_ladoB
+		return par % cant_personas;
+	}
+
+public:
+	int tiempo = 0;
+
+	Escenario(){
+
+	}
+
+	int parPosible() const
+	{
+		int parEvaluar = this.parActual_x_paso[this.paso] + 1;
+
+		while(!parValido(parEvaluar) && parEvaluar<=this.pares_totales){
+			parEvaluar += 1;
 		}
 
- 	public:
-		int parPosible() const
+		if (parEvaluar<=this.tot_pares)
 		{
-			int parEvaluar = parActual_x_paso[paso] + 1;
-
-			while(!parValido(parEvaluar) && parEvaluar<=this.pares_totales){
-				parEvaluar += 1;
-			}
-
-			if (parEvaluar<=tot_pares)
-			{
-				return parEvaluar;
-			}else{
-				return -1;
-			}
+			return parEvaluar;
+		}else{
+			return -1;
 		}
-		int faroleroPosible() const;
-		void enviarPar(const int par);
-		void enviarFarolero(const int persona);
-		void backtrackPar();  	
-		void backtrackFarolero();
+	}
+	int faroleroPosible() const
+	{
+		int faroleroAEvaluar = this.parActual_x_paso[this.paso] + 1;
+
+		while(!faroleroValido(parEvaluar) && faroleroAEvaluar<=this.personas_totales){
+			faroleroAEvaluar += 1;
+		}
+
+		if (faroleroAEvaluar<=this.personas_totales)
+		{
+			return faroleroAEvaluar;
+		}else{
+			return -1;
+		}
+
+	}
+	void enviarPar(const int par){
+		//Guardo el paso que funciono
+		this.parActual_x_paso[this.paso] = par;
+
+		//Recalculo el balance
+		int a  = this.primer(par);
+		int b  = this.segundo(par);
+
+		this.cant_canibales_ladoA -= this.isCanibal(a) - this.isCanibal(b);
+		this.cant_arqueologos_ladoA -= !this.isCanibal(a) - !this.isCanibal(b);
+		this.cant_canibales_ladoB += this.isCanibal(a) + this.isCanibal(b);
+		this.cant_arqueologos_ladoB += !this.isCanibal(a) + !this.isCanibal(b);
+
+		//Marco a las personas como que viajaron al lado B
+		this.personas_ladoA[a] = 0;
+		this.personas_ladoA[b] = 0;
+
+		//Avanzo el paso
+		this.paso += 1;
+		//Reseteo las personas a probar, la primer persona a ser evaluada va a ser la 0
+		this.parActual_x_paso[this.paso] = 0;
+
+		//El par se lleva la lampara al lado B
+		this.tienenLampara = 0;
+
+		this.tiempo += max(this.getTiempoPersona(a),this.getTiempoPersona(b));
+	}
+	void enviarFarolero(const int persona)
+	{
+			//Guardo el paso que funciono
+		this.parActual_x_paso[this.paso] = persona;
+
+			//Recalculo el balance
+
+		this.cant_canibales_ladoA += this.isCanibal(persona);
+		this.cant_arqueologos_ladoA += !this.isCanibal(persona);
+		this.cant_canibales_ladoB -= this.isCanibal(persona);
+		this.cant_arqueologos_ladoB -= !this.isCanibal(persona);
+
+			//Marco a las personas como que viajaron al lado B
+		this.personas_ladoA[persona] = 1;
+
+			//Avanzo el paso
+		this.paso += 1;
+			//Reseteo las tuplas a probar, la primer tupla a probar va a ser la de cardinalidad 0
+		this.parActual_x_paso[this.paso] = 0;
+
+		//El farolero retorna la lampara al lado A
+		this.tienenLampara=1;
+
+		this.tiempo += this.getTiempoPersona(persona);
+
+
+	}
+	void backtrackPar()
+	{
+		//Si no hay más pasos para atras entonces devuelvo false
+		if (this.paso--<0)
+		{
+			return false;
+		} 
+
+		//De lo contrario busco el par que produjo el backtrack y lo hago volver al lado A
+		par = this.parActual_x_paso[this.paso];
+
+		int a  = this.primer(par);
+		int b  = this.segundo(par);
+
+		this.cant_canibales_ladoA += this.isCanibal(a) + this.isCanibal(b);
+		this.cant_arqueologos_ladoA += !this.isCanibal(a) + !this.isCanibal(b);
+		this.cant_canibales_ladoB -= this.isCanibal(a) - this.isCanibal(b);
+		this.cant_arqueologos_ladoB -= !this.isCanibal(a) - !this.isCanibal(b);
+
+		//Marco a las personas como que retornaron al lado A
+		this.personas_ladoA[a] = 1;
+		this.personas_ladoA[b] = 1;
+
+		//Se deshace la ida del par, y con ellos vuelve la lampara al lado A
+		this.tienenLampara=1;
+
+		this.tiempo -= max(this.getTiempoPersona(a),this.getTiempoPersona(b));
+
+		return true;
+	}  	
+	void backtrackFarolero()
+	{
+		//Si no hay más pasos para atras entonces devuelvo false
+		if (this.paso--<0)
+		{
+			return false;
+		} 
+
+		//De lo contrario busco el par que produjo el backtrack y lo hago volver al lado A
+		persona = this.parActual_x_paso[this.paso];
+
+		int a  = this.primer(par);
+		int b  = this.segundo(par);
+
+		this.cant_canibales_ladoA -= this.isCanibal(persona);
+		this.cant_arqueologos_ladoA -= !this.isCanibal(persona);
+		this.cant_canibales_ladoB += this.isCanibal(persona);
+		this.cant_arqueologos_ladoB += !this.isCanibal(persona);
+
+		//Marco a las personas como que retornaron al lado A
+		this.personas_ladoA[persona] = 0;
+
+		//Como se deshizo la vuelta del farolero, entonces en A no tienen lampara
+		this.tienenLampara=0;
+
+		this.tiempo -= this.getTiempoPersona(persona);
+
+		return true;
+	}
+
+	int pasaronTodos()
+	{
+		return cant_canibales_ladoA + cant_arqueologos_ladoA == 0;
+	}
 
 }
--- tp1/ej1.cpp
+++ tp1/ej1.cpp
@@ -5,102 +5,68 @@
 #include <fstream>
 using namespace std;
 
-bool tienenLampara = true;
-int arq_totales;
-int arq_destino = 0;
-int can_totales;
-int can_destino = 0;  
-int * parActual_x_paso;
-int paso = 0;
 
-int * arqueologo_origen;
-int * canibal_origen;
 
 int main(int argc, char* argv[])
 {
+ 	ifstream input( "ej1.in" );
+ 	int cant_arqueologos, cant_canibales;
+ 	input >> cant_arqueologos >> cant_canibales;
+ 	
+ 	int tiempos_arqueologos[cant_arqueologos];
+ 	int tiempos_canibales[cant_canibales];
 
+ 	int i = 0;
+ 	for (i = 0; i < cant_arqueologos; i++)
+ 	{
+ 		tiempos_arqueologos[i];
+ 	}
+ 	for (i = 0; i < cant_arqueologos; i++)
+ 	{
+ 		tiempos_canibales[i];
+ 	}
+
+ 	int t = algoritmoResolucion(cant_arqueologos, cant_canibales, &tiempos_arqueologos, &tiempos_canibales);
+	
+	cout << t;
+}
+
+int algoritmoResolucion(int cant_arqueologos, int cant_canibales, int * tiempos_arqueologos, int * tiempos_canibales)
+{
 	bool exitoBackPar = true;
 	bool exitoBackLampara = true;
+	bool minimo = -1;
+
+	Escenario escenario = Escenario(cant_arqueologos, cant_canibales, tiempos_arqueologos, tiempos_canibales);
 
 	while(exitoBackLampara || exitoBackPar){
-		if (tienenLampara)//flag
+		if (escenario.tienenLampara)//flag
 		{
-			par = parPosible();
+			par = escenario.parPosible();
 			if (par>-1)//Busco en el casillero del paso si me quedan pares posibles
 			{
 				enviarPar(par);//flag + anotacion en arreglos + 1 paso más
 			}else{
-				exitoBackLampara = backtrackRetorno(farolero);//vuelve al paso anterior
+				exitoBackLampara = escenario.backtrackRetorno(farolero);//vuelve al paso anterior
 			}
 		}else{
-			farolero = retornoPosible();
+			farolero = escenario.retornoPosible();
 			if (farolero>-1)//busco en el casillero
 			{
 				retornarLampara(farolero);//flag + anotacion en arreglos + 1 paso más
 			}else{
-				 exitoBackPar = backtrackPar(par);//vuelve al paso anterior y cambia de rama
-				}
+				 exitoBackPar = escenario.backtrackPar(par);//vuelve al paso anterior y cambia de rama
 			}
+		}
 
-			if (pasaronTodos())
+		if (escenario.pasaronTodos())
+		{
+			if (escenario.tiempo < minimo || minimo == -1)
 			{
-				guardarTiempo();
+				minimo = escenario.tiempo;
 			}
 		}
-
-
 	}
 
-	int parPosible(){
-		int parEvaluar = parActual_x_paso[paso] + 1;
-		int tot_pares = arq_totales*can_totales;
-		while(!parValido(parEvaluar) && parEvaluar<=tot_pares){
-			parEvaluar += 1;
-		}
-
-		if (parEvaluar<=tot_pares)
-		{
-			return parEvaluar;
-		}else{
-			return -1;
-		}
-		
-	}
-
-	bool parValido(par){
-		int aux_arq_destino = arq_destino;
-		int aux_can_destino = can_destino;  
-
-		//Checkeo de que no me vaya de los limites (un par valido)
-		if (par>arq_totales*can_totales)
-		{
-			return false;
-		}
-		//parseo los datos de la tupla
-		a = primero(par);
-		b = segundo(par);
-
-		if (!((esCanibal(a) || esArquitecto(a)) && (esCanibal(b) || esArquitecto(b)))) {
-			return false; //persona invalida
-		}
-
-		//checkeo que el balance siga valiendo
-		aux_can_destino += esCanibal(a) && canibal_origen[a];
-		aux_arq_destino += esArquitecto(a) && arquitecto_origen[a];
-
-		aux_can_destino += esCanibal(b) && canibal_origen[b];
-		aux_arq_destino += esArquitecto(b) && arquitecto_origen[b];
-		
-		int arq_origen = arq_totales - aux_arq_destino;
-		int can_origen = can_totales - aux_can_destino;
-
-		return arq_origen >= can_origen && arq_destino>=can_destino;
-	}
-
-void enviarPar(par){
-	parActual_x_paso[paso] = par;
-	tienenLampara = 0;
-	paso++;
-
-	//actualizar estructura
+	return minimo;
 }
