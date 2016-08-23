a7d5a95dee5496e7dacba25991e0fcb079e8295b
Nicolas Carlos Hernandez <nhernandez@ws17.labo4>
2016-08-23 18:57:12 -0300

    fix

--- tp1/ej2/balanza.cpp
+++ tp1/ej2/balanza.cpp
@@ -92,11 +92,13 @@ void Balanza::invertir(int* arreglo, int size){
 	arreglo = otro;
 }
 void Balanza::imprimir(){
-	printf("%d %d\n",this.size_izq, this.size_der);
-	for(int i = 0, i < size_izq, i++){
+	printf("%d %d\n",this->size_izq, this->size_der);
+	for(int i = 0; i < size_izq; i++){
 		printf("%d ", platoIzquierdo[i]);
 	}
-	for(int j = 0, j < size_der, j++){
+	printf("\n");
+	for(int j = 0; j < size_der; j++){
 		printf("%d ", platoDerecho[j]);
 	}
+	printf("\n");
 }
