02e1c2af8187810dffad2267793a314f51314a82
Nicolas Carlos Hernandez <nhernandez@ws17.labo4>
2016-08-23 17:24:42 -0300

    El codeo principal esta listo. A debuguear.

--- tp1/ej2/balanza.cpp
+++ tp1/ej2/balanza.cpp
@@ -28,28 +28,32 @@ void Balanza::balancear(){
 	bool estaEnNegativo = false;
 	int pesaActual = pow(3, i);
 	int n = equilibrioActual - pesaActual;
+	int pesaUno = 1;
 	for( bool terminar = false; !terminar; ){
 		if( n == 0 ){
 			pesasUtilizadas[j] = pesaActual;
 			terminar = true;
-			//cant_pesas = j;
+			cant_pesas = j+1;
 		} else if( abs(n) == 1 ){
 			pesasUtilizadas[j] = pesaActual;
-			pesasUtilizadas[j+1] = 1; //pow(3, 0)
-			//cant_pesas = j+1;
+			pesasUtilizadas[j+1] = pesaUno; //pow(3, 0)
+			cant_pesas = j+2;
 			terminar = true;
 		} else if( abs(n) > 1 && abs(n) < equilibrioActual ){
+			//agrego una pesa y sigo loopeando
 			pesasUtilizadas[j] = pesaActual;
+			j++;
 			i--;
 			//equilibrioActual = n;
 			estaEnNegativo = n < 0; //se setea true si n menor 0
 			equilibrioActual = abs(n);
 		} else if( abs(n) >= equilibrioActual ){
+			//La pesa que trate de usar es muy grande
 			i--;
 		}
 		if( estaEnNegativo ){
 			pesaActual = pow(3,i) * -1;
-			pesaUno = pow(3,0) * -1; //TODO declarar esta variable
+			pesaUno = pow(3,0) * -1; 
 		} else{
 			pesaActual = pow(3, i);
 			pesaUno = pow(3, 0);
@@ -84,4 +88,6 @@ void Balanza::invertir(int* arreglo, int size){
 	for(int i = 0; i < size ; i++){
 		otro[size - i] = arreglo[i];
 	}
+	delete(arreglo);
+	arreglo = otro;
 }
