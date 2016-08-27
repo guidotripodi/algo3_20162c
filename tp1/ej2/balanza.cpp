4c42b2661b305d565cbe23660c0e8e2a3f89a06b
Nicolas Carlos Hernandez <nhernandez@ws8.labo4>
2016-08-26 21:22:35 -0300

    debugging

--- tp1/ej2/balanza.cpp
+++ tp1/ej2/balanza.cpp
@@ -35,11 +35,14 @@ void Balanza::balancear(){
 	int n = equilibrioActual - pesaActual;
 	int pesaUno = 1;
 	for( bool terminar = false; !terminar; ){
+	
 		if( n == 0 ){
+			printf("n == 0\n");
 			pesasUtilizadas[j] = pesaActual;
 			terminar = true;
 			cant_pesas = j+1;
 		} else if( abs(n) == 1 ){
+			printf("n == 1\n");
 			pesasUtilizadas[j] = pesaActual;
 			if( n < 0 ){
 				pesasUtilizadas[j+1] = pesaUno * -1;
@@ -48,14 +51,14 @@ void Balanza::balancear(){
 			}
 			cant_pesas = j+2;
 			terminar = true;
-		} else if( abs(n) > 1 && abs(n) < equilibrioActual ){
+		} else if( abs(n) > 1 && abs(n) < abs(equilibrioActual) ){
 			//agrego una pesa y sigo loopeando
+			//printf("n == 0\n");
 			pesasUtilizadas[j] = pesaActual;
 			j++;
 			i--;
 			equilibrioActual = n;
 			estaEnNegativo = n < 0; //se setea true si n menor 0
-			equilibrioActual = abs(n);
 		} else if( abs(n) >= equilibrioActual ){
 			//La pesa que trate de usar es muy grande
 			i--;
@@ -67,7 +70,7 @@ void Balanza::balancear(){
 			pesaActual = pow(3, i);
 			pesaUno = pow(3, 0);
 		}
-		n = abs(equilibrioActual) - abs(pesaActual);
+		n = equilibrioActual - pesaActual;
 	}
 	armadoBalanza();
 }
