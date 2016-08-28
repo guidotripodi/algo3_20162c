030b5bf8cfd1667391a9f9bc945b54aaf6b49e08
Nicolas <nicoh22@hotmail.com>
2016-08-28 13:05:44 -0300

    Logre poner el invertir.

--- tp1/ej2/ej2-nuevo/ejercicio2.cpp
+++ tp1/ej2/ej2-nuevo/ejercicio2.cpp
@@ -4,6 +4,28 @@
 //con el mismo razonamiento que en la demo las pesas usadas son siempre menos que raiz de P
 FILE *doc;
 
+void imprimir(long long* derecho, long long* izquierdo, int sizeDer, int sizeIzq){
+	fprintf(doc, "%d %d\n",sizeIzq, sizeDer);
+	for( int i = 0; i < sizeIzq; i++ ){
+		fprintf(doc, "%lld ", izquierdo[i]);
+	}
+	fprintf(doc, "\n");
+	for( int j = 0; j < sizeDer; j++ ){
+		fprintf(doc, "%lld ", derecho[j]);
+	}
+	fprintf(doc, "\n");
+}
+
+void invertir(long long* arreglo, int size){
+	if( size < 2 ) return;
+	long long temp; 
+	for( int i = 0; i < size/2 ; i++ ){
+		temp = arreglo[i];
+		arreglo[i] = arreglo[size - i - 1];
+		arreglo[size - i - 1] = temp;
+	}
+}
+
 void balancear(long long  p){
     
 	long long  equilibrioActual = p;
@@ -22,21 +44,31 @@ void balancear(long long  p){
     // armo el arreglo de sumas parciales
     while (i >= 0) {
         sumasParciales[i] = sumaParcial;
-        sumaParcial=(sumaParcial-pow(3,i-1)); // -1 porque el exponente termina en +1 en el while anterior
+		// -1 porque el exponente termina en +1 en el while anterior
+        sumaParcial=(sumaParcial-pow(3,i-1));
         i--;
     }
     
     long long  middle = (size)/2;
-    
+	long long platoDerecho[size];
+	long long platoIzquierdo[size];
+	int j = 0;
+	int k = 0;
+
     while (llabs(equilibrioActual) > 0) {
-        if (sumasParciales[middle] >= llabs(equilibrioActual) && sumasParciales[middle-1] < llabs(equilibrioActual)) {
-        
+        if (sumasParciales[middle] >= llabs(equilibrioActual) &&
+				sumasParciales[middle-1] < llabs(equilibrioActual)) {
+
             long long  potencia = sumasParciales[middle]-sumasParciales[middle-1];
             if (equilibrioActual < 0) {
                 equilibrioActual = potencia + equilibrioActual;
-                printf("plato derecho %lld \n", potencia);
+                platoDerecho[j] = potencia;
+				j++;
+				printf("plato derecho %lld \n", potencia);
             }else {
                 equilibrioActual = equilibrioActual - potencia;
+				platoIzquierdo[k] = potencia;
+				k++;
                 printf("plato izquierdo %lld \n", potencia);
             }
             
@@ -52,14 +84,19 @@ void balancear(long long  p){
             middle = (middle)/2;
         }
     }
+	invertir(platoDerecho, j);
+	invertir(platoIzquierdo, k);
+	imprimir(platoDerecho, platoIzquierdo, j, k);
 }
 
 
 int main(){ 
-    long long i = 5;
+    long long i = 30;
     long long  pesa = 0;
-    while (i > 0) {
-        pesa = pow(10,15); 
+    doc = stdout;
+	while (i > 0) {
+        //pesa = pow(10,i); 
+        pesa = i; 
         printf("pesa : %lld \n", pesa);
         balancear(pesa);
         i--;
