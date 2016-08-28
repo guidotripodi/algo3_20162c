cba52d58b5776abaa0cf1614d1dc978cb5853421
Nicolas <nicoh22@hotmail.com>
2016-08-28 15:41:37 -0300

    command line options

--- tp1/ej2/ej2-nuevo/main.cpp
+++ tp1/ej2/ej2-nuevo/main.cpp
@@ -1,21 +1,29 @@
 //#include "ejercicio2.cpp"
 #include "balanza.h"
 #include <iostream>
-#include <string>
+#include <string.h>
 #include <sstream>
 #include <stdio.h>
 
-int main(){
-
-	std::string entrada;
-	int peso;
-	while( getline(std::cin, entrada) ){
-		std::stringstream(entrada) >> peso;
+int main(int argc, char* argv[]){
+	
+	if(argc > 1){
+		if(strcmp(argv[1], "test") == 0){
+			//tests
+		} else{
+			printf("Bad input\n");
+		}
+	} else{
+		//toma un valor de stdin y lo procesa
+		std::string entrada;
+		int peso;
+		while( getline(std::cin, entrada) ){
+			std::stringstream(entrada) >> peso;
+		}
+		FILE* file = stdout;
+		Balanza b1(peso);
+		b1.imprimir(file);
 	}
-
-	FILE* file = stdout;
-	Balanza b1(peso);
-	b1.imprimir(file);
 
 	return 0;
 }
