0305a38f76ca5f68c2d7f83a29cf91a89d35c1e7
Nicolas Carlos Hernandez <nhernandez@ws6.labo4>
2016-08-29 19:43:37 -0300

    Incluyo las funciones de tests. Uso long long.

--- tp1/ej2/ej2-nuevo/balanza.h
+++ tp1/ej2/ej2-nuevo/balanza.h
@@ -5,7 +5,7 @@
 class Balanza{
 	public:
 		//inicializacion es O(sqrt(p)) si no es todo una mentira
-		Balanza(int p); 
+		Balanza(long long p); 
 		~Balanza();
 		void imprimir(FILE*); 
 		//puedo imprimir por consola o a un archivo
--- tp1/ej2/ej2-nuevo/tests.cpp
+++ tp1/ej2/ej2-nuevo/tests.cpp
@@ -1,12 +1,14 @@
 void test3i(){
 
 	/*Caso P = 3^i*/
+	FILE* archivo = fopen("./test3i.txt", "a");
+	//FILE* archivo = stdout;
 	long long peso = 0;
 	int i = 0;
 	while(i < 30){
 		peso = pow(3,i);
 		Balanza b1(peso);
-		b1.imprimir(stdout);
+		b1.imprimir(archivo);
 		i++;
 	}
 }
@@ -14,6 +16,8 @@ void test3i(){
 void testPSumaDePotencias(){
 
 	/*Caso P = 3^i + i-1 hasta i = 0*/
+	FILE* archivo = fopen("./testSumaPotencias.txt", "a");
+	//FILE* archivo = stdout;
 	long long peso = 0;
 	int i = 0;
 	int x = 0;
@@ -24,7 +28,7 @@ void testPSumaDePotencias(){
 			x++;
 		}
 		Balanza b1(peso);
-		b1.imprimir(stdout);
+		b1.imprimir(archivo);
 		i++;
 	}
 }
@@ -32,6 +36,8 @@ void testPSumaDePotencias(){
 void testPnumerosPares(){
 
 	/*Caso P = par*/
+	FILE* archivo = fopen("./testNumerosPares.txt", "a");
+	//FILE* archivo = stdout;
 	long long peso = 0;
 	int i = 0;
 	while(i < 500){
@@ -39,7 +45,7 @@ void testPnumerosPares(){
 		if(peso % 2 == 0){
 			peso = i;
 			Balanza b1(peso);
-			b1.imprimir(stdout);
+			b1.imprimir(archivo);
 		}
 		i++;
 	}
@@ -49,6 +55,8 @@ void testPnumerosPares(){
 void testPnumerosImpares(){
 
 	/*Caso P = par*/
+	FILE* archivo = fopen("./testNumerosImpares.txt", "a");
+	//FILE* archivo = stdout;
 	long long peso = 0;
 	int i = 0;
 	while(i < 500){
@@ -56,7 +64,7 @@ void testPnumerosImpares(){
 		if(peso % 2 == 1){
 			peso = i;
 			Balanza b1(peso);
-			b1.imprimir(stdout);
+			b1.imprimir(archivo);
 		}
 		i++;
 	}
@@ -66,12 +74,14 @@ void testPnumerosImpares(){
 void test_random(){
 
 	/*Caso P = par*/
+	FILE* archivo = fopen("./tests.txt", "a");
+	//FILE* archivo = stdout;
 	long long peso = 0;
 	int i = 0;
 	while(i < 500){
 		peso = i;	
 		Balanza b1(peso);
-		b1.imprimir(stdout);
+		b1.imprimir(archivo);
 		i++;
 	}
 }
