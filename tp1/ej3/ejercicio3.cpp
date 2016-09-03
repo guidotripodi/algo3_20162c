ae5ead0500433a433077c6837b02dfbc4ef2dd26
rkapobel01 <rok_35@live.com.ar>
2016-09-03 02:14:22 -0300

    ej3

--- tp1/ej3/ejercicio3.cpp
+++ tp1/ej3/ejercicio3.cpp
@@ -11,6 +11,8 @@ int N = 0;
 int *objectsWeights = NULL;
 int *objectsValues = NULL;
 int *knapSacksCapacities = NULL;
+int**** objects3dMatrices = NULL;
+int*** objects2dMatrices = NULL;
 
 int max(int a, int b){
     return (a > b) ? a : b;
@@ -72,188 +74,304 @@ void simpleKnapSackProblem() {
     printf("maximo logrado %d", K[N-1][knapSackCapacity]);
 }
 
-void bidimentionalKnapSackProblem() {
-	
-	int* K[knapSacksCapacities[0] +1][knapSacksCapacities[1] + 1];
-	
-	int max = 0;
-	int k1Max = 0;
-	int k2Max = 0;
-	
-	for(int i = 0; i < N; i++) {
-		int objectWeight = objectsWeights[i];
-	
-		for(int k2 = knapSacksCapacities[1]; k2 >= objectWeight; k2--){
-			for(int k1 = knapSacksCapacities[0]; k1 >= objectWeight; k1--){
-				if(K[k1][k2] == NULL){
-					K[k1][k2] = new int[3](); // maximo, objeto, mochilas
-					K[k1][k2][2] = -1;
-				}
-				
-				if(K[k1-objectWeight][k2][0] > K[k1][k2][0] && K[k1-objectWeight][k2][0] > K[k1][k2-objectWeight][0]){
-					K[k1][k2][0] = K[k1-objectWeight][k2][0] + objectsValues[i];
-					K[k1][k2][1] = i;
-					K[k1][k2][2] = M1;					
-				}else if (K[k1][k2-objectWeight][0] > K[k1][k2][0] && K[k1][k2-objectWeight][0] > K[k1-objectWeight][k2][0]) {
-					K[k1][k2][0] = K[k1][k2-objectWeight][0] + objectsValues[i];
-					K[k1][k2][1] = i;
-					K[k1][k2][2] = M2;
-				}
-				
-				if(K[k1][k2][0] > max) {
-					max = K[k1][k2][0];
-					k1Max = k1;
-					k2Max = k2;
-				}
-			}
-		}
-	}
-	
-	int maxAux = max;
-	
-	printf("Mochilas: \n");
-	
-	while (maxAux > 0) {
-		
-		int knapsack = K[k1Max][k2Max][2];
-		int weight = objectsWeights[K[k1Max][k2Max][1]];
-		int value = objectsValues[K[k1Max][k2Max][1]];
-		printf("objeto %d, %d en mochila %d \n\n", value, weight, knapsack);
-		
-		if(knapsack == M1){
-			k1Max = k1Max - weight;
-		}else {
-			k2Max = k2Max - weight;
-		}
-		
-		maxAux -= value;
-	}
+void create3dMatrices() {
+    
+    int km1 = knapSacksCapacities[0];
+    int km2 = knapSacksCapacities[1];
+    int km3 = knapSacksCapacities[2];
+    
+    for (int i = 0; i < N; i++) {
+        int*** K = new int**[km1 +1];
+        
+        for (int x = 0; x <= km1; x++) {
+            K[x] = new int*[km2 +1];
+            
+            for (int y = 0; y <= km2; y++) {
+                K[x][y] = new int[km3 +1];
+                
+                for (int z = 0; z <= km3; z++) {
+                    K[x][y][z] = 0;
+                }
+            }
+        }
+    
+        objects3dMatrices[i] = K;
+    }
+}
 
-	printf("maximo logrado %d", max);
+void create2dMatrices() {
+    
+    int km1 = knapSacksCapacities[0];
+    int km2 = knapSacksCapacities[1];
+    
+    for (int i = 0; i < N; i++) {
+        int** K = new int*[km1 +1];
+        
+        for (int x = 0; x <= km1; x++) {
+            K[x] = new int[km2 +1];
+            
+            for (int y = 0; y <= km2; y++) {
+                K[x][y] = 0;
+            }
+        }
+        
+        objects2dMatrices[i] = K;
+    }
+}
+
+void copy3dMatrixTo(int i) {
+    
+    int km1 = knapSacksCapacities[0];
+    int km2 = knapSacksCapacities[1];
+    int km3 = knapSacksCapacities[2];
+
+    for (int x = 0; x <= km1; x++) {
+        for (int y = 0; y <= km2; y++) {
+            for (int z = 0; z <= km3; z++) {
+                objects3dMatrices[i][x][y][z] = objects3dMatrices[i-1][x][y][z];
+            }
+        }
+    }
+}
+
+void copy2dMatrixTo(int i) {
+    
+    int km1 = knapSacksCapacities[0];
+    int km2 = knapSacksCapacities[1];
+    
+    for (int x = 0; x <= km1; x++) {
+        for (int y = 0; y <= km2; y++) {
+            objects3dMatrices[i][x][y] = objects3dMatrices[i-1][x][y];
+        }
+    }
 }
 
 void tridimentionalKnapSackProblem() {
 	
-	int* K[knapSacksCapacities[0] +1][knapSacksCapacities[1] + 1][knapSacksCapacities[2] + 1];
-	
-	for (int x = 0; x <= knapSacksCapacities[0]; x++) {
-		for (int y = 0; y <= knapSacksCapacities[1]; y++) {
-			for (int z = 0; z <= knapSacksCapacities[2]; z++) {
-				K[x][y][z] = new int[3](); // maximo, objeto, mochilas
-				K[x][y][z][0] = 0;
-				K[x][y][z][1] = 0;
-				K[x][y][z][2] = -1;
-			}
-		}
-	}
-	
+    int km1 = knapSacksCapacities[0];
+    int km2 = knapSacksCapacities[1];
+    int km3 = knapSacksCapacities[2];
+    
+    create3dMatrices();
+    
 	int max = 0;
 	int k1Max = 0;
 	int k2Max = 0;
 	int k3Max = 0;
-	
-	printf("N es %d", N);
+    int objectMax = 0;
 	
 	int k1 = 0;
 	int k2 = 0;
 	int k3 = 0;
 	
 	for(int i = 0; i < N; i++) {
-		int objectWeight = objectsWeights[i];
-	
-		for(k3 = knapSacksCapacities[2]; k3 >= 0; k3--){
-			for(k2 = knapSacksCapacities[1]; k2 >= 0; k2--){
-				for(k1 = knapSacksCapacities[0]; k1 >= 0; k1--){
-					
-					int m0 = K[k1][k2][k3][0];
+		int weight = objectsWeights[i];
+        int value = objectsValues[i];
+        
+        if (i>0) {
+            copy3dMatrixTo(i);
+        }
+        
+        int ***K = objects3dMatrices[i];
+        
+		for(k3 = km3; k3 >= 0; k3--){
+			for(k2 = km2; k2 >= 0; k2--){
+				for(k1 = km1; k1 >= 0; k1--){
+                    
+					int m0 = K[k1][k2][k3];
 					int m1 = 0;
 					int m2 = 0;
 					int m3 = 0;
 					
-					if(k1 >= objectWeight){
-						m1 = K[k1-objectWeight][k2][k3][0];
+					if(k1 >= weight){
+						m1 = K[k1-weight][k2][k3];
 					}
 					
-					if(k2 >= objectWeight){
-						m2 = K[k1][k2-objectWeight][k3][0];
+					if(k2 >= weight){
+						m2 = K[k1][k2-weight][k3];
 					}
 					 
-					if(k3 >= objectWeight){
-						m3 = K[k1][k2][k3-objectWeight][0];
+					if(k3 >= weight){
+                        m3 = K[k1][k2][k3-weight];
 					}
 					
-					int value = objectsValues[i];
-					
-					if(k1 >= objectWeight && m1 + value >= m0 && 	
+					if(k1 >= weight && m1 + value >= m0 &&
 					   m1 + value >= m2 + value &&
-					   m1 + value >= m3 + value){
-						
-						//printf("entre con valor %d \n\n", K[k1-objectWeight][k2][k3][0] + objectsValues[i]);
+					   m1 + value >= m3 + value) {
 				
-						K[k1][k2][k3][0] = m1 + value;
-						K[k1][k2][k3][1] = i;
-						K[k1][k2][k3][2] = M1;	
+						K[k1][k2][k3] = m1 + value;
 											
-					}else if (k2 >= objectWeight && m2 + value >= m0 && 	
+					}else if (k2 >= weight && m2 + value >= m0 &&
 					          m2 + value >= m1 + value &&
 					          m2 + value >= m3 + value) {
 						
-						//printf("entre con valor %d \n\n", K[k1][k2-objectWeight][k3][0] + objectsValues[i]);
+						K[k1][k2][k3] = m2 + value;
 						
-						K[k1][k2][k3][0] = m2 + value;
-						K[k1][k2][k3][1] = i;
-						K[k1][k2][k3][2] = M2;
-					
-					}else if (k3 >= objectWeight && m3 + value >= m0 && 	
+					}else if (k3 >= weight && m3 + value >= m0 &&
 					          m3 + value >= m1 + value &&
-					          m3 + value >= m2 + value) {
+                              m3 + value >= m2 + value) {
 					
-						//printf("entre con valor %d \n\n", K[k1][k2][k3-objectWeight][0] + objectsValues[i]);
-					
-						K[k1][k2][k3][0] = m3 + value;
-						K[k1][k2][k3][1] = i;
-						K[k1][k2][k3][2] = M3;
-					
+						K[k1][k2][k3] = m3 + value;
+                        
 					}
-				
 					
-					printf ("maximo obtenido %d con objeto %d, %d \n", K[k1][k2][k3][0], objectsValues[K[k1][k2][k3][1]], objectsWeights[K[k1][k2][k3][1]]);
-				
-					if(K[k1][k2][k3][0] > max) {
-						max = K[k1][k2][k3][0];
+					if(K[k1][k2][k3] > max) {
+						max = K[k1][k2][k3];
 						k1Max = k1;
 						k2Max = k2;
 						k3Max = k3;
+                        objectMax = i;
 					}
 				}
 			}
 		}
 	}
 	
-	int maxAux = max;
-	
-	printf("Mochilas: \n");
-	
-	while (maxAux > 0) {
-		
-		int knapsack = K[k1Max][k2Max][k3Max][2];
-		int weight = objectsWeights[K[k1Max][k2Max][k3Max][1]];
-		int value = objectsValues[K[k1Max][k2Max][k3Max][1]];
-		printf("k1: %d, k2: %d, k3: %d \n", k1Max, k2Max, k3Max);
-		printf("objeto %d, %d en mochila %d \n\n", value, weight, knapsack);
-		
-		if(knapsack == M1){
-			k1Max = k1Max - weight;
-		}else if(knapsack == M2){
-			k2Max = k2Max - weight;
-		}else {
-			k3Max = k3Max - weight;
-		}
-		
-		maxAux -= value;
-	}
-	
+    
+    int i = objectMax;
+    
+    while (i > 0) {
+        int weight = objectsWeights[i];
+        int value = objectsValues[i];
+        
+        int ***Ki = objects3dMatrices[i];
+        int ***Kj = objects3dMatrices[i-1]; // j = i-1
+        
+        // no uso el actual (i) sigo con i-1
+        if (!(Ki[k1Max][k2Max][k3Max] == Kj[k1Max][k2Max][k3Max])) {
+            // entiendo que seguro sucede que: kj[k1Max][k2Max][k1Max] + objectsValue[i] = kj[k1Max][k2Max][k1Max]
+            if (k1Max >= weight && Kj[k1Max-weight][k2Max][k3Max] + value == Ki[k1Max][k2Max][k3Max]) {
+                k1Max -= weight;
+                printf("objeto %d, %d en mochila de %d kg \n\n", value, weight, km1);
+            }else if(k2Max >= weight && Kj[k1Max][k2Max-weight][k3Max] + value == Ki[k1Max][k2Max][k3Max]) {
+                k2Max -= weight;
+                printf("objeto %d, %d en mochila de %d kg \n\n", value, weight, km2);
+            }else {
+                k3Max -= weight;
+                printf("objeto %d, %d en mochila de %d kg \n\n", value, weight, km3);
+                // deberia ser obligatorio...por alguna vino
+            }
+        }
+        
+        i--;
+    }
+    
+    if (i == 0 && max) {
+        int weight = objectsWeights[0];
+        int value = objectsValues[0];
+        
+        if (k1Max >= weight) {
+            printf("objeto %d, %d en mochila de %d kg \n\n", value, weight, km1);
+        }else if (k2Max >= weight) {
+            printf("objeto %d, %d en mochila de %d kg \n\n", value, weight, km2);
+        }else {
+            printf("objeto %d, %d en mochila de %d kg \n\n", value, weight, km3);
+        }
+    }
+
 	printf("maximo logrado %d \n", max);
+}
+
+void bidimentionalKnapSackProblem() {
+    
+    int km1 = knapSacksCapacities[0];
+    int km2 = knapSacksCapacities[1];
+    
+    create2dMatrices();
+    
+    int max = 0;
+    int k1Max = 0;
+    int k2Max = 0;
+    int objectMax = 0;
+    
+    int k1 = 0;
+    int k2 = 0;
+    
+    for(int i = 0; i < N; i++) {
+        int weight = objectsWeights[i];
+        int value = objectsValues[i];
+        
+        if (i>0) {
+            copy2dMatrixTo(i);
+        }
+        
+        int **K = objects2dMatrices[i];
+    
+        for(k2 = km2; k2 >= 0; k2--){
+            for(k1 = km1; k1 >= 0; k1--){
+                
+                int m0 = K[k1][k2];
+                int m1 = 0;
+                int m2 = 0;
+                
+                if(k1 >= weight){
+                    m1 = K[k1-weight][k2];
+                }
+                
+                if(k2 >= weight){
+                    m2 = K[k1][k2-weight];
+                }
+                
+                if(k1 >= weight && m1 + value >= m0 &&
+                   m1 + value >= m2 + value) {
+                    
+                    K[k1][k2] = m1 + value;
+                    
+                }else if (k2 >= weight && m2 + value >= m0 &&
+                          m2 + value >= m1 + value) {
+                    
+                    K[k1][k2] = m2 + value;
+                    
+                }
+                
+                if(K[k1][k2] > max) {
+                    max = K[k1][k2];
+                    k1Max = k1;
+                    k2Max = k2;
+                    objectMax = i;
+                }
+            }
+        }
+    }
+    
+    
+    int i = objectMax;
+    
+    while (i > 0) {
+        int weight = objectsWeights[i];
+        int value = objectsValues[i];
+        
+        int **Ki = objects2dMatrices[i];
+        int **Kj = objects2dMatrices[i-1]; // j = i-1
+        
+        // no uso el actual (i) sigo con i-1
+        if (!(Ki[k1Max][k2Max] == Kj[k1Max][k2Max])) {
+            // entiendo que seguro sucede que: kj[k1Max][k2Max][k1Max] + objectsValue[i] = kj[k1Max][k2Max][k1Max]
+            if (k1Max >= weight && Kj[k1Max-weight][k2Max] + value == Ki[k1Max][k2Max]) {
+                k1Max -= weight;
+                printf("objeto %d, %d en mochila de %d kg \n\n", value, weight, km1);
+            }else {
+                k2Max -= weight;
+                printf("objeto %d, %d en mochila de %d kg \n\n", value, weight, km2);
+                // deberia ser obligatorio...por alguna vino
+            }
+        }
+        
+        i--;
+    }
+    
+    if (i == 0 && max) {
+        int weight = objectsWeights[0];
+        int value = objectsValues[0];
+        
+        if (k1Max >= weight) {
+            printf("objeto %d, %d en mochila de %d kg \n\n", value, weight, km1);
+        }else {
+            printf("objeto %d, %d en mochila de %d kg \n\n", value, weight, km2);
+        }
+    }
+    
+    printf("maximo logrado %d \n", max);
 }
 
 int main(){
@@ -273,8 +391,21 @@ int main(){
     objectsWeights = weights;
     objectsValues = values;
     knapSacksCapacities = knapSacks;
- 
- 	tridimentionalKnapSackProblem();
+    
+    if (M == 1) {
+        simpleKnapSackProblem();
+    }else if (M == 2) {
+        if (knapSacksCapacities[0] == knapSacksCapacities[1]) {
+            //habria que calcular dos veces el simple.....pero para esto hay que modificar un toque el codigo.
+            //hay que marcar objetos usados.
+        }else {
+            objects2dMatrices = new int**[N];
+            bidimentionalKnapSackProblem();
+        }
+    }else {
+        objects3dMatrices = new int***[N];
+        tridimentionalKnapSackProblem();
+    }
     
 	return 0;
 }
