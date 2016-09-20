8f0074bee62f003782b3f1af76a225ebad89a2e6
Nicolas Carlos Hernandez <nhernandez@ws18.labo4>
2016-09-20 20:29:45 -0300

    El problema estaba en el constructor del grafo

--- tp2/ej3/grafo.cpp
+++ tp2/ej3/grafo.cpp
@@ -4,7 +4,7 @@ Grafo::Grafo(int* aristas, int cantNodos, int cantAristas){
 	int origen, destino, peso;
 	//for(int i = 0; i < cantAristas; i = i + 3){
 	int i = 0;
-	while(i < cantAristas){
+	while(i < 3*cantAristas){
 		origen = aristas[i] -1;
 		destino = aristas[i + 1] - 1;
 		peso = aristas[i + 2];
