ea09a24eb3b2f7670663e4ccb47978ae71db98cf
Nicolas Carlos Hernandez <nhernandez@ws18.labo4>
2016-09-20 19:52:58 -0300

    sigo debugueando errores tontos

--- tp2/ej3/grafo.cpp
+++ tp2/ej3/grafo.cpp
@@ -2,11 +2,14 @@
 Grafo::Grafo(int* aristas, int cantNodos, int cantAristas){ 
 	adyacencia = new std::list<Arista>[cantNodos];
 	int origen, destino, peso;
-	for(int i = 0; i < cantAristas; i++){
+	//for(int i = 0; i < cantAristas; i = i + 3){
+	int i = 0;
+	while(i < cantAristas){
 		origen = aristas[i] -1;
 		destino = aristas[i + 1] - 1;
 		peso = aristas[i + 2];
 		adyacencia[origen].push_back(Arista(destino, peso));
+		i = i + 3;
 	}
 }
 
