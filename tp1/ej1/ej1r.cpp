4eba87dbf14340c76885396f9c56c95ece35bfd4
Esteban <estebanlucianorey@gmail.com>
2016-09-26 01:06:16 -0300

    Se esta desarrollando la estructura e chequeo historico para el algoritmo

---
+++ tp1/ej1/ej1r.cpp
@@ -0,0 +1,105 @@
+//Recibir los parametros como esta en el enunciado
+//Generar todas las tuplas posibles
+// A los pares los identifico por el numero de fila y columna que representan en una matriz donde las filas son los arqueologos seguidos por los canibales
+#include <iostream>
+#include <fstream>
+#include "Escenario2.hpp"
+using namespace std;
+
+
+int algoritmoResolucion(int cant_arqueologos, int cant_canibales, int * tiempos_arqueologos, int * tiempos_canibales);
+
+int main(int argc, char* argv[])
+{
+	//ifstream input( "ej1.in" );
+	//ifstream input = stdin;
+	int cant_arqueologos, cant_canibales;
+	cin >> cant_arqueologos >> cant_canibales;
+
+
+	int tiempos_arqueologos[cant_arqueologos];
+	int tiempos_canibales[cant_canibales];
+
+	int i = 0;
+	for (i = 0; i < cant_arqueologos; i++)
+	{
+		cin >> tiempos_arqueologos[i];
+	}
+	for (i = 0; i < cant_canibales; i++)
+	{
+		cin >> tiempos_canibales[i];
+	}
+//	cout<<"Entrada inicializada: comenzando algoritmo...\n";
+	int t = algoritmoResolucion(cant_arqueologos, cant_canibales, tiempos_arqueologos, tiempos_canibales);
+	
+	cout <<t<<"\n";
+
+	return 1;
+}
+
+int algoritmoResolucion(int cant_arqueologos, int cant_canibales, int * tiempos_arqueologos, int * tiempos_canibales)
+{
+	bool exitoBackPar = true;
+	bool exitoBackLampara = true;
+	int minimo = -1; 
+//	cout<<"Inicializando escenario...\n";
+	Escenario2 escenario = Escenario2(cant_arqueologos, cant_canibales, tiempos_arqueologos, tiempos_canibales);
+//	cout<<"Escenario listo\n";
+	while(exitoBackLampara && exitoBackPar){
+//		cout<<"----------------------------------------------\nNuevo paso:\n";
+		escenario.printStatus();
+
+		//De haber encontrado una solucion, obtengo el tiempo logrado
+		if (escenario.pasaronTodos())
+		{
+			if (escenario.tiempo < minimo || minimo == -1)
+			{
+				minimo = escenario.tiempo;
+			}
+
+			cout<<" < Fin de rama: minimo logrado = "<<escenario.tiempo<<" > \n";
+		}
+
+		exitoBackPar = true;
+		exitoBackLampara = true;
+		if (escenario.tienenLampara)
+		{
+			Escenario2::Eleccion eleccion = escenario.envioPosible();
+			//Si hay un par posible y si la rama que estoy evaluando
+			//me sigue dando una mejor solucion a la ya encontrada
+			if (eleccion.posible && (minimo == -1 || escenario.tiempo<minimo))
+			{
+//				cout<<"-Enviando ";
+//				escenario.printPar(par);
+//				cout<<"\n";
+
+				escenario.enviarEleccion(eleccion);
+			}else{
+				//vuelve al paso anterior
+//				cout<<"-Backtracking a farolero\n";
+				exitoBackLampara = escenario.deshacerRetorno();
+
+			}
+		}else{
+			Escenario2::Eleccion eleccion = escenario.retornoPosible();
+
+			//Si hay un farolero que pueda hacer que retorne y que me mantenga el tiempo menor al ya encontrado
+			if (eleccion.posible && (minimo == -1 || escenario.tiempo<minimo))
+			{
+				
+				escenario.retornarEleccion(eleccion);
+
+			}else{
+//				cout<<"-Backtracking a par\n";
+				exitoBackPar = escenario.deshacerEnvio();
+
+			}
+		}
+
+		
+	}
+
+//	cout<<"Soluciones encontradas: "<<sol<<"\n";
+
+	return minimo;
+}
