acaa7d770438c28ac53b1d238f3c30c5e9e813c0
Nicolas Hernandez <nicoh22@hotmail.com>
2016-10-25 16:35:50 -0300

    3opt. Creo.

--- tp3/ej3/ej3.cpp
+++ tp3/ej3/ej3.cpp
@@ -216,26 +216,44 @@ vector<int> mejorar2opt(vector<int> solucionParcial){
 	return solucion;
 }
 
+/* Mi error fue tratar de asociar el concepto de arista a un nodo (el destino)
+ * Al eliminar las 3 aristas que elegimos quedan dos(o tres?) maneras de reconectar el camino
+ * sin que sea un camino 2opt. Sean 0 -> a  b->c d->fin aristas y las borramos.
+ * Caso1: Reconectamos 0 -> b, a -> d y c -> fin (intervalos (a,b) (c,d) invertidos)
+ * Caso2: Reconectamos 0 -> c, d -> a y b -> fin ("swap" de rangos sin invertir)
+ * Caso3: Reconectamos 0 -> d, c -> a y b -> fin (swap + invertir rango (c,d))
+ * Caso4: Reconectamos 0 -> c, d -> b y a -> fin (swap + invertir rango (a,b))
+ *
+ * Nota: 0 no es el principio del arreglo y fin no es el fin del arreglo
+ * solo indican principio y fin del subarreglo que deberia cambiar.
+ * TODO: analizar casos borde!!!
+ * Propiedades utiles para la implementacion: 
+ * 0 = a-1
+ * b = c-1
+ * d = fin - 1
+ * me deja usar 3 subindices*/
+				
 vector<int> mejorar3opt(vector<int> solucionParcial){
 	vector<int> solucion = solucionParcial;
 	long long costoAnterior = calcularCosto(solucionParcial);
 	int cantNodos = solucionParcial.size();
 	long long costoActual;
 	
-    // fijate como recorro:
+    
+	// fijate como recorro:
     // lo mas chica que puede ser la entrada para 3opt es de 5 eltos: 1->2->3->4->5 y no se recorre asi nomas
     // [0...i-1][i...j-1][j...k-1][k...size-1]
     for (int i = 1; i < cantNodos-3; i++) {
         for (int j = i+1; j < cantNodos-2; j++) {
 			for (int k = j+2; k < cantNodos; k++) {
 				
+				//rango 1: i a j
+				//rango 2: j+1 a k 
+
 				//Caso 1
-				// hay que intercambiar el intervalo i..j-1 por j..k-1 SIN invertir los intervalos! si no queda un 2opt!
-				// calcular el costo
-				// rehacer! 
-				
-				// invertir como digo
-				
+				reverse(solucionParcial.begin() + i, solucionParcial.begin() + j);
+				reverse(solucionParcial.begin() + j+1, solucionParcial.begin() + k);//aca estoy asumiendo que el nodo c 
+
 				costoActual = calcularCosto(solucionParcial);
 
 				if (costoActual != -1 && costoActual < costoAnterior) {
@@ -244,8 +262,52 @@ vector<int> mejorar3opt(vector<int> solucionParcial){
 					printf("Costo mejorado: %lld\n", costoActual);
 				}
 				
-				// rehacer!
+				reverse(solucionParcial.begin() + i, solucionParcial.begin() + j);
+				reverse(solucionParcial.begin() + j+1, solucionParcial.begin() + k);
+
+				//Caso 2
+
+				reverse(solucionParcial.begin() + i, solucionParcial.begin() + k);
+				reverse(solucionParcial.begin() + i, solucionParcial.begin() + i + (k - j+1));//len(rango 2)
+				reverse(solucionParcial.begin() + (k - j+1)+ 1, solucionParcial.begin() + k);
+
+				costoActual = calcularCosto(solucionParcial);
+
+				if (costoActual != -1 && costoActual < costoAnterior) {
+					costoAnterior = costoActual;
+					solucion = solucionParcial;
+				}
+				reverse(solucionParcial.begin() + i, solucionParcial.begin() + i + (k - j+1));//len(rango 2)
+				reverse(solucionParcial.begin() + (k - j+1)+ 1, solucionParcial.begin() + k);
+				reverse(solucionParcial.begin() + i, solucionParcial.begin() + k);
 				
+				//Caso 3
+				
+				reverse(solucionParcial.begin() + i, solucionParcial.begin() + k);
+				reverse(solucionParcial.begin() + i, solucionParcial.begin() + i + (k - j+1));//len(rango 2)
+				
+				costoActual = calcularCosto(solucionParcial);
+
+				if (costoActual != -1 && costoActual < costoAnterior) {
+					costoAnterior = costoActual;
+					solucion = solucionParcial;
+				}
+				
+				reverse(solucionParcial.begin() + i, solucionParcial.begin() + i + (k - j+1));//len(rango 2)
+				reverse(solucionParcial.begin() + i, solucionParcial.begin() + k);
+				
+				//Caso 4
+				reverse(solucionParcial.begin() + i, solucionParcial.begin() + k);
+				reverse(solucionParcial.begin() + (k - j+1)+ 1, solucionParcial.begin() + k);
+
+				costoActual = calcularCosto(solucionParcial);
+
+				if (costoActual != -1 && costoActual < costoAnterior) {
+					costoAnterior = costoActual;
+					solucion = solucionParcial;
+				}
+				reverse(solucionParcial.begin() + (k - j+1)+ 1, solucionParcial.begin() + k);
+				reverse(solucionParcial.begin() + i, solucionParcial.begin() + k);
 			}
 
 		}
