3f3589d8959e760cb04442bcfb28a16029da5177
Esteban <estebanlucianorey@gmail.com>
2016-10-30 20:59:08 -0300

    Se arregla el generador de test random

--- tp3/ej1-test/case-gen.py
+++ tp3/ej1-test/case-gen.py
@@ -2,7 +2,7 @@ from pylab import *
 
  # Como utilizar
  #Para armar una instancia hay que determinar:
- #			- Un método que posicione los elementos : Seccion GENERADORES DE POSICIONES
+ #			- Un metodo que posicione los elementos : Seccion GENERADORES DE POSICIONES
  #			- Un metodo que asigne de determinada forma las pociones a los gimnasios: Seccion GENERADORES DE POCIONES
  #			- Un metodo que genere lainstancia con lo anterior: Seccion INSTANCIAS
  #			
@@ -14,15 +14,17 @@ from pylab import *
 #			GENERADORES DE POSICIONES
 def randomPositions(n, notin=[]):
 	li = []
-	for x in xrange(1,n):	
-		t = 2*pi*random()
-		r = sqrt(random())
-		v =(r*cos(t), r*sin(t))
+	x = 1
+	while len(li)<n:	
+		t = 2*pi*rand()
+		r = sqrt(randint(0,1000))
+		v =(int(r*cos(t)), int(r*sin(t)))
+
 		if v in notin or v in li:
-			x = x - 1
 			continue
 
 		li.append(v)
+
 	return li	
 
 def circularPositions(rad_circ, n):
@@ -32,7 +34,7 @@ def circularPositions(rad_circ, n):
 		r = rad_circ[r_i]	
 		for y in xrange(0,obj_x_circ):
 			t = 2*pi/obj_x_circ * y
-			v = (r*cos(t), r*sin(t))
+			v = (int(r*cos(t)), int(r*sin(t)))
 			
 			li.append(v)
 	
@@ -57,8 +59,8 @@ def randomPotions(gyms, pps, k_bag):
 
 ###############################################
 #				UTILITARIOS
-def plotInstance(instance):
-	i = instance()
+def plotInstance(i):
+
 	for ((x, y), p) in i[0]:
 	    plot(x, y, 'ro')
 	
@@ -66,6 +68,27 @@ def plotInstance(instance):
 	    plot(x, y, 'bo')
 	
 	show()
+
+def saveInstance(instance, file, mode="w"):
+	g = instance[0]
+	p = instance[1]
+	m = instance[2]
+
+	target = open(file, mode)
+
+	target.write(str(len(g))+" "+str(len(p))+" "+str(m)+"\n")
+
+	for gym in g:
+		target.write(str(gym[0][0])+" "+str(gym[0][1])+" "+str(gym[1])+"\n")
+	for pp in p:
+		target.write(str(pp[0])+" "+str(pp[1])+"\n")
+	
+	target.close()
+
+
+
+
+
 
 
 ###############################################
@@ -77,12 +100,13 @@ def plotInstance(instance):
  #		  #gyms y #pp random
  #		  mochila K=10
 def randomInstance(n):
-	cg = randint(0,n)
+	cg = randint(1,n-1)
+	mochila = 10
 	gyms = randomPositions(n-cg)
-	pps = randomPositions(n/2, gyms)
-	gymsD = randomPotions(gyms, pps, 10)
+	pps = randomPositions(n-len(gyms), gyms)
+	gymsD = randomPotions(gyms, pps, mochila)
 
-	return gymsD, pps
+	return gymsD, pps, mochila
 
  #Disposicio circular: Determinado numero de gimnasios y pp
  #		  Posiciones en forma de anillos concentricos de radios 2, 1.5 1 0.5
@@ -90,23 +114,24 @@ def randomInstance(n):
  #		  #gyms y #pp random
  #		  mochila K=10
 def circularInstance(cant_gyms, cant_pp):
+	mochila = 10
 	gyms = circularPositions([1, 2], cant_gyms)
 	pps = circularPositions([0.5, 1.5], cant_pp)
-	gymsD = randomPotions(gyms, pps, 10)
+	gymsD = randomPotions(gyms, pps, mochila)
 
-	return gymsD, pps
+	return gymsD, pps, mochila
 
 
  #Mochila de capacidad 0: determinado numero de gimnasios y pp
  #		  Posiciones en forma de anillos concentricos de radios 2, 1.5 1 0.5
  #		  Pociones random pero con solucion
  #		  mochila K=0
-def noBagInstance(cant_gyms, cant_pp)
+def noBagInstance(cant_gyms, cant_pp):
 	gyms = circularPositions([1, 2], cant_gyms)
 	pps = circularPositions([0.5, 1.5], cant_pp)
 	gymsD = randomPotions(gyms, pps, 0)
 
-	return gymsD, pps 
+	return gymsD, pps, 0 
 
  #Todos gimnasios: Determinado N (se parametriza cuanto de uno y de otro)
  #		  Posiciones en forma de anillos concentricos de radios 2, 1.5 1 0.5
@@ -116,13 +141,17 @@ def noBagInstance(cant_gyms, cant_pp)
 def noPPInstance(n):
 	gyms = randomPositions(n)
 	gymsD = randomPotions(gyms, pps, 10)
+	return gymsD, [], 10
 
 
 
 ###############################################
 #				MAIN
 
+#plotInstance(circularInstance(100, 3))
 
-
-plotInstance(randomInstance)
-plotInstance(circularInstance)
+for i in xrange(3,1000):
+	r = randomInstance(i)
+	#plotInstance(r)
+	saveInstance(r, "test1.in","a")
+	pass
