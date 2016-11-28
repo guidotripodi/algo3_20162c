1378410434a6f2099d0508ba327a422bbaf67645
guidotripodi <guido.tripodi@hotmail.com>
2016-11-27 21:07:12 -0300

    arriba medicion OK.

--- rtp3/ej2-test/random.cpp
+++ rtp3/ej2-test/random.cpp
@@ -20,7 +20,7 @@ int main(int argc, char* argv[])
 	cout << "Entrada" << "," << "Medicion" << "," << "Data" << "\n";
 	int cantidadElementos = 0;
 	int cant_aux = 0;
-	int cantidadInstancia = 1;
+	int cantidadInstancia = 0;
 	long long medicion = 0;
 	long long data = 0;
 	for (int j = 0; j < 2168; ++j)
@@ -50,7 +50,8 @@ int main(int argc, char* argv[])
 			pp_aux[i] = posicion;
 
 		}
-		cant_aux = cant_pokeParadas + cant_gimnasios;
+		cout << cant_pokeParadas + cant_gimnasios << ",";
+/*		cant_aux = cant_pokeParadas + cant_gimnasios;
 		if (cantidadElementos == 0)	{
 			cantidadElementos = cant_pokeParadas + cant_gimnasios;
 		}
@@ -81,14 +82,14 @@ int main(int argc, char* argv[])
 		auto start = ya();
 		f = algoritmoResolucion(cant_gimnasios, cant_pokeParadas, cap_mochila, posiciones_gym, posiciones_pp, pp_aux);
 		auto end = ya();
-		medicion += chrono::duration_cast<std::chrono::nanoseconds>(end-start).count();
+		cout << chrono::duration_cast<std::chrono::nanoseconds>(end-start).count() << ",";
 	
 		if ( f == NULL || f->first == -1)	{
 			//cout << "-1" << "\n";
 			//return -1;
 		}else{
 			//cout << f->first <<" ";//<< f->second->size();
-			data += f->first;
+			cout << f->first << "\n";
 			/*for (std::list<int>::iterator it=f->second->begin(); it != f->second->end(); ++it){
 				cout << " " << *it;
 			}*/
